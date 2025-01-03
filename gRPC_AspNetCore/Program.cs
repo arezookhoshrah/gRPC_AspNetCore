f528d1019efa701c6dace5aac841bd8efce2cfdc
arezookhoshrah <arezoo.khoshrah@gmail.com>
2025-01-03 22:53:06 +0330

    feat:add rpc method for product service

--- gRPC_AspNetCore/Program.cs
+++ gRPC_AspNetCore/Program.cs
@@ -1,14 +1,21 @@
+
+using gRPC_AspNetCore.Context;
 using gRPC_AspNetCore.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddDbContext<GrpcContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("GrpcConnection"));
+});
 // Add services to the container.
 builder.Services.AddGrpc();
-
+builder.Services.AddScoped<GrpcProductService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+app.MapGrpcService<GrpcProductService>();
+
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
---
+++ gRPC_AspNetCore/Services/GrpcProductService.cs
@@ -0,0 +1,130 @@
+﻿using Azure.Core;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using gRPC_AspNetCore.Context;
+using gRPC_AspNetCore.Models;
+using gRPC_AspNetCore.Protos;
+using Microsoft.EntityFrameworkCore;
+using static gRPC_AspNetCore.Protos.ProductService;
+
+namespace gRPC_AspNetCore.Services
+{
+    public class GrpcProductService(GrpcContext ctx):ProductServiceBase
+    {
+        public override async Task CreateProduct(IAsyncStreamReader<CreateProductRequest> requestStream, IServerStreamWriter<CreateProductReply> responseStream, ServerCallContext context)
+        {
+            int createdProductsCount = 0;
+            while(await requestStream.MoveNext())
+            {
+                ctx.Products.Add(new Product
+                {
+                    Title = requestStream.Current.Title,
+                    Description = requestStream.Current.Description,
+                    Price = requestStream.Current.Price,
+                    CreateDate = DateTime.Now
+                });
+
+                createdProductsCount++;
+            }
+
+            await ctx.SaveChangesAsync();
+
+            await responseStream.WriteAsync(new CreateProductReply
+            {
+                CreatedItemsCount = createdProductsCount,
+                Message = "Products created successfully",
+                Status = 200
+            });
+        }
+
+        public override async Task<GetProductByIdReply> GetProductById(GetProductByIdRequest request, ServerCallContext context)
+        {
+            Product? product = await ctx.Products.FirstOrDefaultAsync(p => p.Id == request.Id);
+
+            if (product is null)
+                return new GetProductByIdReply();
+
+            return new GetProductByIdReply
+            {
+                Id = product.Id,
+                Title = product.Title,
+                Description = product.Description,
+                Price = product.Price,
+                CreateDate = Timestamp.FromDateTime(product.CreateDate)
+            };
+        }
+
+        public override async Task GetAllProducts(GetAllProductsRequest request, IServerStreamWriter<GetAllProductsReply> responseStream, ServerCallContext context)
+        {
+            int skip = request.Take * request.Page;
+
+            List<Product> products = await ctx.Products
+                                              .Skip(skip)
+                                              .Take(request.Take)
+                                              .ToListAsync();
+
+            foreach(var product in products)
+            {
+                await responseStream.WriteAsync(new GetAllProductsReply
+                {
+                    Id = product.Id,
+                    Title = product.Title,
+                    Description = product.Description,
+                    Price = product.Price,
+                    CreateDate = Timestamp.FromDateTime(product.CreateDate)
+                });
+            }
+        }
+
+        public override async Task<RemoveProductByIdReply> RemoveProductById(IAsyncStreamReader<RemoveProductByIdRequest> requestStream, ServerCallContext context)
+        {
+            int removeItemsCount = 0;
+
+            while (await requestStream.MoveNext())
+            {
+                Product? product = await ctx.Products.FirstOrDefaultAsync(p => p.Id == requestStream.Current.Id);
+
+                if (product is null)
+                    continue;
+
+                ctx.Products.Remove(product);
+
+                removeItemsCount++;
+            }
+            await ctx.SaveChangesAsync();
+
+            return new RemoveProductByIdReply
+            {
+                Message = "Remove Product Successfully Done",
+                RemovedItemsCount = removeItemsCount,
+                Status = 200
+            };
+
+        }
+
+        public override async Task<UpdateProductReply> UpdateProduct(UpdateProductRequest request, ServerCallContext context)
+        {
+            Product? product = await ctx.Products.FirstOrDefaultAsync(p => p.Id == request.Id);
+
+            if (product is null)
+            {
+                // return null;
+                throw new NullReferenceException();
+            }
+
+            product.Title = request.Title;
+            product.Description = request.Description;
+            product.Price = request.Price;
+
+            ctx.Products.Update(product);
+            await ctx.SaveChangesAsync();
+
+            return new UpdateProductReply
+            {
+                Message = "Update Product Successfully Done",
+                Status = 200,
+                UpdatedItemsCount = 1
+            };
+        }
+    }
+}
